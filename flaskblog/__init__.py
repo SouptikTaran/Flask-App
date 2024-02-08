b0122df0c17b69242fb9fa0cfdbeef7abbe04cb2
ft.souptik <mr.souptiktaran@gmail.com>
2024-02-08 11:20:23 +0530

    v0.1.0: A Basic Web App, based on Flask

---
+++ flaskblog/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e7e4723092e01cbc7a90832644e1b77f001b18e1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+from flaskblog import routes
+
+
