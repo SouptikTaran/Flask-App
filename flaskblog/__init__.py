511d2dcda6a4953ea14231e531654ecc18d023ff
ft.souptik <mr.souptiktaran@gmail.com>
2024-02-16 13:52:08 +0530

    v0.2.0: Added Post creation , delete and update functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e7e4723092e01cbc7a90832644e1b77f001b18e1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
